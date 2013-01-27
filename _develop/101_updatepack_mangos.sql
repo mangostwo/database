068ade65d4ab0fc2d90a60322f77b4c996566d26
Salja <salja2012@hotmail.de>
2013-01-27 09:58:37 +0100

    [0003] Fix Server Startup Errors
    Signed-off-by: Salja <salja2012@hotmail.de>

--- _develop/101_updatepack_mangos.sql
+++ _develop/101_updatepack_mangos.sql
@@ -17,6 +17,10 @@
 -- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
 
+-- Fix Server Startup Errors
+DELETE FROM `creature_addon` WHERE auras IN (46598, 43671, 50628, 52349);
+DELETE FROM `creature_template_addon` WHERE auras IN (46598, 43671, 50628);
+
 -- Cleanup
 UPDATE gameobject SET state = 0 WHERE id IN (SELECT entry FROM gameobject_template WHERE type = 0 AND data0 = 1);
 UPDATE creature_template SET unit_flags=unit_flags&~2048 WHERE unit_flags&2048=2048;
