a53810dcb3c734195e76089c243af9070958022b
Salja <salja2012@hotmail.de>
2013-02-20 00:19:05 +0100

    [0010] Some Updates for Coming Updatepack
    Signed-off-by: Salja <salja2012@hotmail.de>

--- _develop/101_corepatch_mangos.sql
+++ _develop/101_corepatch_mangos.sql
@@ -47,3 +47,11 @@ CREATE TABLE `dbscripts_on_creature_death` (
 ALTER TABLE db_version CHANGE COLUMN required_12349_01_mangos_dbscripts_on_creature_death required_12364_01_mangos_spell_script_target bit;
 
 ALTER TABLE spell_script_target ADD COLUMN `inverseEffectMask` mediumint(8) unsigned NOT NULL DEFAULT '0' AFTER `targetEntry`;
+
+ALTER TABLE db_version CHANGE COLUMN required_12364_01_mangos_spell_script_target required_12370_01_mangos_npc_vendor bit;
+
+ALTER TABLE npc_vendor ADD COLUMN `condition_id` mediumint(8) unsigned NOT NULL default '0' AFTER ExtendedCost;
+
+ALTER TABLE db_version CHANGE COLUMN required_12370_01_mangos_npc_vendor required_12370_02_mangos_npc_vendor_template bit;
+
+ALTER TABLE npc_vendor_template ADD COLUMN `condition_id` mediumint(8) unsigned NOT NULL default '0' AFTER ExtendedCost;
