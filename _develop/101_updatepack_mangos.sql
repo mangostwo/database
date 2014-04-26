c3a0bc01bda221ff6b7b98330a602a1b2c13efcd
sanctum32 <andriuspel@gmail.com>
2014-04-26 11:34:20 +0300

    Added fix for 11919 and 11940 quests provided by xfurry.

--- _develop/101_updatepack_mangos.sql
+++ _develop/101_updatepack_mangos.sql
@@ -267,5 +267,19 @@ DELETE FROM creature_questrelation WHERE id NOT IN (SELECT entry FROM creature_t
 DELETE FROM creature_onkill_reputation WHERE creature_id NOT IN (SELECT entry FROM creature_template);
 UPDATE creature_template SET npcflag=npcflag|2 WHERE entry IN (SELECT id FROM creature_questrelation UNION SELECT id FROM creature_involvedrelation);
 
+-- 11919 and 11940 fix (by xfurry)
+DELETE FROM db_script_string WHERE entry IN (2000005623,2000005624,2000005625,2000005626);
+INSERT INTO db_script_string (entry,content_default,sound,type,language,emote,comment) VALUES
+(2000005623,'Easy now, drakeling.',0,0,0,0,'Raelorasz - say_drake_1'),
+(2000005624,'A wonderful specimen.',0,0,0,0,'Raelorasz - say_drake_2'),
+(2000005625,'Sleep now, young one....',0,0,0,0,'Raelorasz - say_drake_3'),
+(2000005626,'Yes, this one should advance my studies nicely....',0,0,0,0,'say_drake_4');
+DELETE FROM dbscripts_on_spell WHERE id IN (46702,46693);
+INSERT INTO dbscripts_on_spell (id, delay, command, datalong, data_flags, dataint, dataint2, dataint3, comments) VALUES
+(46702, 1, 15, 46704, 6, 0, 0, 0, 'cast Raelorasz Fireball'),
+(46702, 1, 0, 0, 2, 2000005623, 2000005624, 2000005625, 'say text'),
+(46702, 5, 0, 0, 2, 2000005626, 0, 0, 'say text'),
+(46693, 0, 14, 46691, 2, 0, 0, 0, 'remove Drake Hatchling Subdued aura');
+
 --UPDATE Database Version
 UPDATE db_version set version = 'TwoDatabase 1.0.1 for MaNGOSTwo XXXXX+ and ScriptDev2 XXXXX+';
