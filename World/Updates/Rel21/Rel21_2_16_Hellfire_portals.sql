5bd7106a00c5fc100aa9276cd0edb8789031ed2b
Charles A Edwards <charles.a.edwards@btinternet.com>
2016-09-07 13:34:58 +0100

    Added short and long descriptions to update
    Added short and long descriptions to update

--- World/Updates/Rel21/Rel21_2_16_Hellfire_portals.sql
+++ World/Updates/Rel21/Rel21_2_16_Hellfire_portals.sql
@@ -24,6 +24,20 @@ BEGIN
     SET @cNewVersion = '21';
     SET @cNewStructure = '2';
     SET @cNewContent = '16';
+                            -- DESCRIPTION IS 30 Characters MAX    
+    SET @cNewDescription = 'Hellfire portals';
+
+                        -- COMMENT is 150 Characters MAX
+    SET @cNewComment = 'Hellfire portals';
+
+    -- Evaluate all settings
+    SET @cCurResult := (SELECT description FROM db_version ORDER BY `version` DESC, STRUCTURE DESC, CONTENT DESC LIMIT 0,1);
+    SET @cOldResult := (SELECT description FROM db_version WHERE `version`=@cOldVersion AND `structure`=@cOldStructure AND `content`=@cOldContent);
+    SET @cNewResult := (SELECT description FROM db_version WHERE `version`=@cNewVersion AND `structure`=@cNewStructure AND `content`=@cNewContent);
+
+    IF (@cCurResult = @cOldResult) THEN    -- Does the current version match the expected version
+        -- APPLY UPDATE
+        START TRANSACTION;
  
         -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
         -- -- PLACE UPDATE SQL BELOW -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
