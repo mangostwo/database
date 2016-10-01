d3b41a8d860ae3177b347c0ee1fc525b2acb6bbe
necrovoice <dragonsfrost@hotmail.com>
2016-09-30 20:15:29 -0400

    removed a type on Rel21_02_40

--- World/Updates/Rel21/Rel21_02_40_Eye_of_Acherus.sql
+++ World/Updates/Rel21/Rel21_02_40_Eye_of_Acherus.sql
@@ -57,9 +57,9 @@ BEGIN
         -- -- PLACE UPDATE SQL BELOW -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
         -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
-    +-- Eye of Acherus - missing aura 
- +DELETE FROM creature_template_addon WHERE entry = 28511;
- +INSERT INTO creature_template_addon VALUES (28511,0,0,1,1,0,0,'51890 51892');
+ -- Eye of Acherus - missing aura 
+ DELETE FROM creature_template_addon WHERE entry = 28511;
+ INSERT INTO creature_template_addon VALUES (28511,0,0,1,1,0,0,'51890 51892');
     
 
         -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
