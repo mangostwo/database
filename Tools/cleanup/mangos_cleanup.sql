b325cb75655ef6ae66344ef51ca5bb3baffca216
Antz <billy1arm@users.noreply.github.com>
2020-05-15 19:36:48 +0100

    Some minor corrections

--- Tools/cleanup/mangos_cleanup.sql
+++ Tools/cleanup/mangos_cleanup.sql
@@ -1,6 +1,6 @@
 --
--- Copyright (C) 2005-2013 MaNGOS <http://getmangos.com/>
--- Copyright (C) 2009-2013 MaNGOSTwo <https://github.com/mangostwo>
+-- Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
+-- Copyright (C) 2009-2020 MaNGOSTwo <https://github.com/mangostwo>
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
