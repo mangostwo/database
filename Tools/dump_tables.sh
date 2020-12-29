8cf11c3dedee0d85e285c35089326665a8225f72
Antz <billy1arm@users.noreply.github.com>
2020-12-29 21:19:45 +0000

    Release 22

--- Tools/dump_tables.sh
+++ Tools/dump_tables.sh
@@ -160,8 +160,7 @@ for TABLE in \
 echo "Dumping ${i}/139 ${TABLE}..."
 
 echo "--
--- Copyright (C) 2005-2017 MaNGOS <http://getmangos.eu/>
--- Copyright (C) 2009-2020 MaNGOSTwo <https://github.com/mangostwo>
+-- Copyright (C) 2005-2021 MaNGOS <http://getmangos.eu/> <https://github.com/mangostwo>
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
