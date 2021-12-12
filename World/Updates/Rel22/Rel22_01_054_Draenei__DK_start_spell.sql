d93d66e91e58e58be864919e1009972abaf50167
Necrovoice <dragonsfrost@hotmail.com>
2021-12-11 20:15:27 -0500

    Minor Update to previous commit.

--- World/Updates/Rel22/Rel22_01_054_Draenei__DK_start_spell.sql
+++ World/Updates/Rel22/Rel22_01_054_Draenei__DK_start_spell.sql
@@ -46,7 +46,7 @@ BEGIN
 	-- Wrong Spell (Shadow Resistance (Racial Passive)) removed from Draenei Death Knight 
 	-- 59221 - SPELLFAMILY_WARRIOR
 	-- 59539 - SPELLFAMILY_DEATHKNIGHT - correct
-	DELETE FROM playercreateinfo_spell WHERE race = 11 AND class = 6 AND Spell = 59221;
+	DELETE FROM `playercreateinfo_spell` WHERE `race` = 11 AND `class` = 6 AND `Spell` = 59221;
 
         -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
         -- -- PLACE UPDATE SQL ABOVE -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
